af589ea2b4c852cd0478b51e5e641de662da7b4c
wowyomad <vadimsun8837@gmail.com>
2022-04-03 23:24:55 +0300

    Initial

--- main.cpp
+++ main.cpp
@@ -13,9 +13,17 @@ int main()
 {
     List *beg;
     List *end;
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout << i;
+    }
+    
 }
 
 void CreateList(List **beg, List **end)
 {
-    
+    *beg = new List;
+    *end = new List;
+
+
 }

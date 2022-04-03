4dbee91f1722c362988a25ee8602b2f473441b54
wowyomad <vadimsun8837@gmail.com>
2022-04-03 20:39:51 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct List
+{
+    int data;
+    List *prev;
+    List *next;
+};
+
+void CreateList(List **beg, List **end);
+
+int main()
+{
+    List *beg;
+    List *end;
+}
+
+void CreateList(List **beg, List **end)
+{
+    
+}
